1bb130a564bfde368103ca5faef19920efe9d1f4
anujaverma11 & Hooman Bargrizan <pair+anujaverma11.hoomangriz@devbootcamp.com>
2015-09-18 15:11:25 -0700

    Create working key generator, hotel can create guests and assign keys

--- luxe/config/routes.rb
+++ luxe/config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
 
-  get '/login' => 'sessions#new'
+  get '/login' => 'sessions#hotel'
+  get '/' => 'sessions#guest'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
   get '/signup' => 'hotels#new'
   post '/hotels' => 'hotels#create'
+
   # get "log_out" => "sessions#destroy", :as => "log_out"
   # get "log_in" => "sessions#new", :as => "log_in"
   # get "sign_up" => "hotels#new", :as => "sign_up"
