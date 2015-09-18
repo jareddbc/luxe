303dd52cd435f348b6377efe688a3f01a633296b
Jared Sobol <email": null>
2015-09-17 17:27:12 -0700

    remove config.action_mailer.default_url_options from env and pdt bc not using devise

--- luxe/db/schema.rb
+++ luxe/db/schema.rb
@@ -15,10 +15,13 @@ ActiveRecord::Schema.define(version: 20150917205721) do
 
   create_table "hotels", force: :cascade do |t|
     t.string   "name"
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
     t.text     "amenities"
     t.string   "location"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "items", force: :cascade do |t|
