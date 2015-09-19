e6e9592d99c400372f57219f35187708cc0eb738
anujaverma11 & Hooman Bargrizan <pair+anujaverma11.hoomangriz@devbootcamp.com>
2015-09-18 22:15:16 -0700

    Fix routing when guests are created, fix routing for guest login, started working on creating and listing services for a hotel

--- luxe/db/schema.rb
+++ luxe/db/schema.rb
@@ -33,8 +33,8 @@ ActiveRecord::Schema.define(version: 20150917205721) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
+    t.string   "address"
     t.text     "amenities"
-    t.string   "location"
     t.boolean  "terms",           default: false
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
