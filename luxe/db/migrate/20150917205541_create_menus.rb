00357b50ff4283c9f438d412a2e2712bb35143c7
anujaverma11 & Hooman Bargrizan <pair+anujaverma11.hoomangriz@devbootcamp.com>
2015-09-19 19:54:31 -0700

    Create and fix Edit and Delete fxn for Menus. Links now will only show when items are present

--- luxe/db/migrate/20150917205541_create_menus.rb
+++ luxe/db/migrate/20150917205541_create_menus.rb
@@ -3,8 +3,6 @@ class CreateMenus < ActiveRecord::Migration
     create_table :menus do |t|
       t.string :name
       t.string :description
-      t.references :services
-      # Why do we have this?
       t.references :hotel, index: true, foreign_key: true
 
       t.timestamps null: false
