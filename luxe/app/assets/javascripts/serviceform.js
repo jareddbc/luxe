75ecf6b2e997811084153f72dced5b4dc3be87d2
Erin Beitel <erin.beitel@gmail.com>
2015-09-20 20:13:30 -0700

    neil the genius helped us angular

--- luxe/app/assets/javascripts/serviceform.js
+++ luxe/app/assets/javascripts/serviceform.js
@@ -1,28 +1,27 @@
-console.log("here")
-angular.module('ui.bootstrap.demo', ['ngAnimate', 'ui.bootstrap']);
-angular.module('ui.bootstrap.demo').controller('AccordionDemoCtrl', function ($scope) {
-  $scope.oneAtATime = true;
+angular.module('luxe')
+  .controller('AccordionDemoCtrl', function ($scope) {
+    $scope.oneAtATime = true;
 
-  $scope.groups = [
-    {
-      title: 'Dynamic Group Header - 1',
-      content: 'Dynamic Group Body - 1'
-    },
-    {
-      title: 'Dynamic Group Header - 2',
-      content: 'Dynamic Group Body - 2'
-    }
-  ];
+    $scope.groups = [
+      {
+        title: 'Dynamic Group Header - 1',
+        content: 'Dynamic Group Body - 1'
+      },
+      {
+        title: 'Dynamic Group Header - 2',
+        content: 'Dynamic Group Body - 2'
+      }
+    ];
 
-  $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+    $scope.items = ['Item 1', 'Item 2', 'Item 3'];
 
-  $scope.addItem = function() {
-    var newItemNo = $scope.items.length + 1;
-    $scope.items.push('Item ' + newItemNo);
-  };
+    $scope.addItem = function() {
+      var newItemNo = $scope.items.length + 1;
+      $scope.items.push('Item ' + newItemNo);
+    };
 
-  $scope.status = {
-    isFirstOpen: true,
-    isFirstDisabled: false
-  };
-});
+    $scope.status = {
+      isFirstOpen: true,
+      isFirstDisabled: false
+    };
+  });
