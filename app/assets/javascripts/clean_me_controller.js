8c6be88b727a8ba6c05472d3f05c193114f710ca
Zoe Ingram <zoe.a.ingram@gmail.com>
2015-09-23 14:41:53 -0700

    adds styling

--- app/assets/javascripts/clean_me_controller.js
+++ app/assets/javascripts/clean_me_controller.js
@@ -2,17 +2,20 @@
 
 angular.module('luxe').controller('CleanMeController', [ '$scope', '$timeout', '$http', function ($scope, $timeout, $http) {
     $scope.date = new Date();
-    $scope.addScheduleCleanService = function(){
+    $scope.addScheduleCleanService = function(cleanData){
 
     var payload = {
       service: {
+        title: 'Room Cleaning',
         starts_at_date : $scope.date,
       }
     };
 
+
+
     $http.post('/api/services', payload).then(
       function(response){
-
+        debugger
         $scope.alert('CleanMe', 'Your service was scheduled!', payload);
       },
       function(response){
--- app/assets/javascripts/datetimeoptions.js
+++ app/assets/javascripts/datetimeoptions.js
@@ -4,34 +4,12 @@ angular.module('luxe')
   .controller('DateTimePickerDemoCtrl',
     function ($scope, $timeout) {
       $scope.dateTimeNow = function() {
-        $scope.date = new Date();
       };
-
-      if(!$scope.date){
-        console.log('Creating new Scope Date');
-        $scope.dateTimeNow();
-      } else {
-        console.log('Using Parent Scope Date');
-      }
-
-      
-      $scope.toggleMinDate = function() {
-        $scope.minDate = $scope.minDate ? null : new Date();
-      };
-       
-      $scope.maxDate = new Date('2014-06-22');
-      $scope.toggleMinDate();
 
       $scope.dateOptions = {
         startingDay: 1,
         showWeeks: false
       };
-      
-      // Disable weekend selection
-      $scope.disabled = function(calendarDate, mode) {
-        return mode === 'day' && ( calendarDate.getDay() === 0 || calendarDate.getDay() === 6 );
-      };
-      
       $scope.hourStep = 1;
       $scope.minuteStep = 15;
 
@@ -44,11 +22,11 @@ angular.module('luxe')
       $scope.timeToggleMode = function() {
         $scope.showMeridian = !$scope.showMeridian;
       };
-      
+
       $scope.$watch("date", function(value) {
         console.log('New date value:' + value);
       }, true);
-      
+
       $scope.resetHours = function() {
         $scope.date.setHours(1);
       };
