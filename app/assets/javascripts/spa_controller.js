5c0eaf8387de116eb1e371671cfc18a1dc500c7f
Zoe Ingram <zoe.a.ingram@gmail.com>
2015-09-23 17:47:04 -0700

    add updates to fix scheduling date

--- app/assets/javascripts/spa_controller.js
+++ app/assets/javascripts/spa_controller.js
@@ -1,23 +1,58 @@
 //= require ./luxe
-
 angular.module('luxe').controller('SpaController', [ '$scope', '$timeout', '$http', function ($scope, $timeout, $http) {
-    $scope.date = new Date();
-    $scope.addScheduleSpaService = function(){
+    $scope.date = new Date;
+    $scope.spa_services = [];
 
+  $scope.thumbnail_selected = function(thumbnail){
+     console.log(thumbnail);
+  }
+
+  function build_items_list(){
+     var items = [];
+     for(var i = 0; i < $scope.available_thumbnails.length; i++){
+       if($scope.available_thumbnails[i].selected){
+         items.push($scope.available_thumbnails[i].data);
+       }
+     }
+     return items;
+  }
+
+  $scope.available_thumbnails = [
+    {
+      data: "Massage",
+      src: "http://oasismassagekaty.com/wp-content/uploads/2014/05/massage1.jpg",
+      selected: false,
+    },
+    {
+      data: "Facial",
+      src: "http://static1.squarespace.com/static/5111ce01e4b0e92fdd98def7/t/52683c55e4b02ae7c37ce382/1360567757761/img-facial-therapies-3.jpg",
+      selected: false,
+    },
+    {
+      data: "Rose Bath",
+      src: "https://s-media-cache-ak0.pinimg.com/736x/73/8f/75/738f757292b289076c5b3e21296c456f.jpg",
+      selected: false,
+    },
+    {
+      data: "Manicure",
+      src: "http://s3.amazonaws.com/brandrepup/note_attachments/32056/3f1ada57d96438412dd2409522778d63362bea97/original/Nail_Services.jpg?1401896372",
+      selected: false,
+    }
+  ];
+  $scope.addScheduleSpaService = function(){
+    var date = $scope.date;
     var payload = {
       service: {
         title: 'Spa Service',
-        starts_at_date : $scope.date,
+        starts_at_date : date,
       }
     };
     $http.post('/api/services', payload).then(
       function(response){
-
-        $scope.alert('Spa', 'Your service was scheduled!', payload);
-
+        $scope.alert('Spa', 'Your service was scheduled for '+date);
       },
       function(response){
-        $scope.alert('Spa', 'FAILED TO SAVE scheduled clean service');
+        $scope.alert('Spa', 'FAILED TO SAVE scheduled spa service');
       }
     );
   }
--- app/controllers/api/services_controller.rb
+++ app/controllers/api/services_controller.rb
@@ -1,16 +1,11 @@
 class Api::ServicesController < ApplicationController
-
 	before_filter do
-
     puts "BEFORE FILTER"
-
     p session[:guest_id]
-
     unless session[:guest_id]
       puts "RENDERING ERROR"
     	render json:  {:error => "Not Logged In"}
     end
-
 	end
 
   def create
@@ -21,36 +16,18 @@ class Api::ServicesController < ApplicationController
     if @service.save
     	begin
         send_text_message @guest.phone, render_to_string('created_message.text')
-      rescue => e 
+      rescue => e
       	p "ERROR SENDING TEXT MESSAGE"
-      	# p e
+
       end
     	render json:  {:error => nil, :service => @service}
     else
     	render json:  {:error => "Failed to create Service", :errors => @service.errors}
     end
-    
+
   end
 
-  # def show
-  #   @service = Service.find_by(id: params[:id])
-  # end
-
-  # def update
-  #   @guest = Guest.find(session[:guest_id])
-  #   @service = Service.find(params[:id])
-  #   @service.update_attributes(:title => params[:title], :starts_at_date => params[:starts_at_date], :starts_at_time => params[:starts_at_time], :special_request => params[:special_request])
-  #   redirect_to @guest
-  # end
-
-  # def destroy
-  #   @service = Service.find_by(id: params[:id])
-  #   @service.destroy
-  #   redirect_to :back
-  # end
-
   private
-
   def service_params
     p params
     params.require(:service).permit(:title, :starts_at_date, :starts_at_time, :special_request)
